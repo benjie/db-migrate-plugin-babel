ce450fb45dd5739e058e92d13197501f9f29632e
Benjie Gillam <benjie@jemjie.com>
2016-10-08 19:44:06 +0100

    A few tweaks requested by Tobias

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-module.exports = {
+// If you're looking to write your own db-migrate plugin, you may be interested
+// in https://github.com/db-migrate/pkgkit
+
+Object.assign(exports, {
   name: 'babel',
   hooks: [
     'migrator:migration:hook:require',
   ],
-  'migrator:migration:hook:require': function() {
-    require('babel-register');
+  loadPlugin: function() {
+    exports['migrator:migration:hook:require'] = function() {
+      require('babel-register');
+    };
+
+    // loadPlugin can be called multiple times, so we unload it here to save precious cycles
+    // https://github.com/db-migrate/node-db-migrate/pull/388#issuecomment-252415666
+    delete exports['loadPlugin'];
   },
-  loadPlugin: function() {},
-};
+});
